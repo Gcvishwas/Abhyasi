1d2c5f44e8c59f1bde3bd45d5611cbef09ede376
Gcvishwas <gcvishwas17@gmail.com>
2025-03-06 12:57:28 +0545

    Created this to check if user is authenticated or not and then render mainlayout

--- src/layouts/protected-routes.tsx
+++ src/layouts/protected-routes.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import LoaderPage from "@/routes/loader-page"
+import { useAuth } from "@clerk/clerk-react" // custom hook provided by clerk with isLoaded and isSigned Values
+import { Navigate } from "react-router-dom"
 
-const ProtectRoutes = () => {
+
+const ProtectRoutes = ({ children }: { children: React.ReactNode }) => {
+    // isLoaded : if authentication has finished loading i.e. User authenticated?
+    // isSigned : if User is signed in
+    const { isLoaded, isSignedIn } = useAuth()
+
+    if (!isLoaded) {
+        return <LoaderPage />
+    }
+
+    if (!isSignedIn) {
+        return <Navigate to={"/signin"} replace />
+    }
     return (
         <div>
-
+            {children}
         </div>
     )
 }
