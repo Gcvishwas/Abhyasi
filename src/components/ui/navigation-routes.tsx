e1aa9ed2633cddde917499bd47b99487736e3ace
Gcvishwas <gcvishwas17@gmail.com>
2025-03-10 23:34:25 +0545

    Built header components

---
+++ src/components/ui/navigation-routes.tsx
@@ -0,0 +1,35 @@
+import { MainRoutes } from "@/lib/helpers"
+import { cn } from "@/lib/utils"
+import { NavLink } from "react-router-dom"
+
+interface NavigationRoutesProps {
+    isMobile?: boolean
+}// interfaces ensure type safety
+
+// in JSX we can simply write the below codes but user will not know if props are passed incorrectly i.e. someone can do isMobile="yes"
+
+const NavigationRoutes = ({ isMobile = false }: NavigationRoutesProps) => {
+    return (
+
+        <ul className={cn("flex items-center gap-6")}>
+            {MainRoutes.map(route => (
+                <NavLink
+                    key={route.href}
+                    to={route.href}
+                    className={({ isActive }) => cn(
+                        "text-base text-neutral-600",
+                        isActive && "text-neutral-900 font-semibold"
+                    )}
+                >
+                    {route.label}
+                </NavLink>
+            ))}
+
+
+        </ul>
+
+
+    )
+}
+
+export default NavigationRoutes
