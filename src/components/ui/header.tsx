72fed2bc19e8e4845e54694310532fdaee6de6d3
Gcvishwas <gcvishwas17@gmail.com>
2025-04-24 14:41:05 +0545

    Added Profile Containers and Toggle Containers to check user status and display header accordingly

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -3,6 +3,9 @@ import { useAuth } from '@clerk/clerk-react'
 import Container from '@/components/ui/container'
 import LogoContainer from './logo-container';
 import NavigationRoutes from './navigation-routes';
+import { NavLink } from 'react-router-dom';
+import ProfileContainer from './profile-container';
+import ToggleContainer from './toggle-container';
 
 
 const Header = () => {
@@ -24,9 +27,31 @@ const Header = () => {
                     <nav className={cn("hidden md:flex items-center gap-3")}>
 
                         <NavigationRoutes />
+                        {/* Checks if user is signed in and if so, show the Take Interview link */}
+                        {userId &&
+                            (
+                                <NavLink
+                                    to={"/generate"}
+                                    className={({ isActive }) => cn(
+                                        "text-lg text-neutral-600",
+                                        isActive && "text-neutral-900 font-semibold"
+                                    )}
+                                >
+                                    Take Interview
+                                </NavLink>
+                            )}
                     </nav>
-                    {/* profile section */}
 
+
+                    <div className='ml-auto flex items-center gap-6'>
+                    </div>
+                    {/* Displays the profile container and mobile toggler where mobile toggler is a hamburger menu and profile container is a user button */}
+
+                    {/* profile section */}
+                    <ProfileContainer />
+
+                    {/* Mobile Toggler */}
+                    <ToggleContainer />
                 </div>
             </Container>
 
--- src/components/ui/navigation-routes.tsx
+++ src/components/ui/navigation-routes.tsx
@@ -11,13 +11,17 @@ interface NavigationRoutesProps {
 const NavigationRoutes = ({ isMobile = false }: NavigationRoutesProps) => {
     return (
 
-        <ul className={cn("flex items-center gap-6")}>
+        <ul className={cn("flex items-center gap-6", isMobile && "items-start flex-col gap-8")}>
+            {/* takes element from helper.ts to map in header */}
             {MainRoutes.map(route => (
+                // Navlink is similar to anchor tag in html but it is used for react router dom
+                // Remember it as <a href="route.href">route.label</a> in html
+                // isActive is a boolean value and it is used to check if the route is active or not
                 <NavLink
                     key={route.href}
                     to={route.href}
                     className={({ isActive }) => cn(
-                        "text-base text-neutral-600",
+                        "text-lg text-neutral-600",
                         isActive && "text-neutral-900 font-semibold"
                     )}
                 >
