b72ffd55fa3ecb4b68ae6917b477c285f384b625
Gcvishwas <gcvishwas17@gmail.com>
2025-05-03 23:21:46 +0545

    Created component for handling authentication and storing user data

---
+++ src/handlers/auth-handler.tsx
@@ -0,0 +1,64 @@
+import { db } from "@/config/firebase.config";
+import LoaderPage from "@/routes/loader-page";
+import { User } from "@/types";
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+// AuthHandler component => responsible for handling authentication and storing user data in local storagex
+const AuthHandler = () => {
+
+    //hook to get authentication status and user data
+    const { isSignedIn } = useAuth();
+
+    //hook to set user data in context
+    const { user } = useUser();
+
+
+    // Get current pathname
+    const pathname = useLocation().pathname;
+
+    //Hook to navigate programmatically
+    const navigate = useNavigate();
+
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const storeUserData = async () => {
+            if (isSignedIn && user) {
+                setLoading(true);
+                try {
+                    const userSnap = await getDoc(doc(db, "users", user.id));
+                    if (!userSnap.exists()) {
+                        const userData: User =
+                        {
+                            id: user.id,
+                            name: user.fullName || user.firstName || "Anonymous",
+                            email: user.primaryEmailAddress?.emailAddress || "No email",
+                            imageURL: user.imageUrl || "",
+                            createdAt: serverTimestamp(),
+                            updatedAt: serverTimestamp(),
+                        };
+                        await setDoc(doc(db, "users", user.id), userData);
+                    }
+                }
+                catch (error) {
+                    console.error("Error storing user data in local storage:", error);
+                }
+                finally {
+                    setLoading(false);
+                }
+            }
+        }
+        storeUserData();
+    }, [isSignedIn, user, pathname, navigate])
+    if (loading)
+        return <LoaderPage />
+
+    //returns null cuz does not render anything to UI
+    return null
+
+};
+
+export default AuthHandler;
