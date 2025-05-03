f83204d24496b7e1201802369816cbd8e6cb21a5
Gcvishwas <gcvishwas17@gmail.com>
2025-05-03 23:20:28 +0545

    Added authhandler

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -2,10 +2,12 @@
 import Header from '@/components/ui/header'
 import Footer from '@/components/ui/footer'
 import { Outlet } from 'react-router-dom'
+import AuthHandler from '@/handlers/auth-handler'
 const PublicLayout = () => {
     return (
         <div className='w-full'>
             {/* handler to store the user data */}
+            <AuthHandler />
             <Header />
             <Outlet />
             <Footer />
