b8cae90bdee3afdcac3c600456e93bd4dd57af96
Gcvishwas <gcvishwas17@gmail.com>
2025-05-03 23:19:25 +0545

    Properly imported keys from .env file

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -3,12 +3,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 
@@ -16,7 +17,8 @@ const firebaseConfig = {
 // to determine if we need to initialize a new app or use the existing one.
 // If there are no initialized apps, we call initializeApp() to create a new app instance.
 
-const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+// console.log('firebaseConfig.projectId:', firebaseConfig.projectId);
 
 // Initialize Firestore
 // Firestore is a cloud-hosted NoSQL database that lets you store and sync data between your users in real time.    
