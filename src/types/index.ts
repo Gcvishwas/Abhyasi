5513e8467e2d39d483a6f577027cd77a9398be15
Gcvishwas <gcvishwas17@gmail.com>
2025-05-23 16:27:30 +0545

    Created schema for InterviewId

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,3 +8,15 @@ export interface User {
     createdAt: Timestamp | FieldValue;
     updatedAt: Timestamp | FieldValue;
 }
+
+export interface Interview {
+    id: string;     // unique identifier for the interview
+    position: string; // job position for the interview
+    description: string; // description of the interview
+    experience: number; // years of experience required
+    userId: string; // ID of the user who created the interview
+    techStack: string;
+    questions: { question: string, answer: string }[]; // array of questions and answers
+    createdAt: Timestamp; // timestamp of when the interview was created
+    updatedAt: Timestamp; // timestamp of when the interview was last updated
+}
