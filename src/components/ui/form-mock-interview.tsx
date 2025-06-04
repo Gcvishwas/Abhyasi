8a9a81198ac76687cc518567b2c70f82f29cd5fa
Gcvishwas <gcvishwas17@gmail.com>
2025-06-04 14:37:03 +0545

    Added AI response for creating interviews

--- src/components/ui/form-mock-interview.tsx
+++ src/components/ui/form-mock-interview.tsx
@@ -9,11 +9,15 @@ import { useAuth } from "@clerk/clerk-react"
 import { toast } from "sonner"
 import Headings from "./headings"
 import { Button } from "./button"
-import { Trash2 } from "lucide-react"
+import { Loader, Trash2 } from "lucide-react"
 import Separator from "./separator"
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "./form"
 import { Input } from "./input"
 import { Textarea } from "./textarea"
+import chatSession from "@/scripts"
+import { json } from "stream/consumers"
+import { addDoc, collection, doc, serverTimestamp, updateDoc } from "firebase/firestore"
+import { db } from "@/config/firebase.config"
 interface FormMockInterviewProps {
     initialData: Interview | null
 }
@@ -21,10 +25,11 @@ interface FormMockInterviewProps {
 const formSchema = z.object({
     position: z.string().min(1, "Position is required")
         .max(100, "Position must be less than 100 characters"),
-    description: z.string().min(10, " Description is required"),
+    description: z.string().min(1, " Description is required"),
     experience: z.coerce.number().min(0, "Experience must be a positive number"),
     techStack: z.string().min(1, "Tech Stack is required"),
 });
+
 
 
 // used to extract the type from the schema eg: type FormData = {
@@ -42,8 +47,8 @@ const FormMockInterview = ({ initialData }: FormMockInterviewProps) => {
     })
 
 
-
-    const { isValid, isSubmitted } = form.formState;
+    //formState is an object that contains the state of the form, such as whether it is valid, dirty, touched, etc.
+    const { isValid, isSubmitting } = form.formState;
     const [loading, setLoading] = useState(false);
     //useNavigate is useful for redirecting the user to a different page after form submission or any other action.
     const navigate = useNavigate();
@@ -58,9 +63,89 @@ const FormMockInterview = ({ initialData }: FormMockInterviewProps) => {
     const toastMessage = initialData ? { title: " Updated..!", description: "Mock Interview updated successfully" } : { title: " Created..!", description: "Mock Interview created successfully" };
 
 
+    const cleanAiResponse = (response: string) => {
+        // Clean the AI response to ensure it is a valid JSON array
+        let cleanText = response.trim();
+        // Remove any occurences of  "json" or code block symbols (` or)
+        cleanText = cleanText.replace(/(json|```|)`/g, "");
+        //Extract the JSON array from the cleaned text between square brackets
+        const jsonArrayMatch = cleanText.match(/\[.*\]/s);
+        if (jsonArrayMatch) {
+            cleanText = jsonArrayMatch[0];
+        } else {
+            throw new Error("Invalid AI response format. Expected a JSON array.");
+        }
+        //Parse the clean JSON array as text into an array of objects
+        try {
+            return JSON.parse(cleanText);
+        } catch (error) {
+            throw new Error("Invalid JSON format:" + (error as Error)?.message);
+        }
+    }
+    const generateAiResponse = async (data: FormData) => {
+        const prompt = `
+        As an experienced prompt engineer, generate a JSON array containing 5 technical interview questions along with detailed answers based on the following job information. Each object in the array should have the fields "question" and "answer", formatted as follows:
+
+        [
+          { "question": "<Question text>", "answer": "<Answer text>" },
+          ...
+        ]
+
+        Job Information:
+        - Job Position: ${data?.position}
+        - Job Description: ${data?.description}
+        - Years of Experience Required: ${data?.experience}
+        - Tech Stacks: ${data?.techStack}
+
+        The questions should assess skills in ${data?.techStack} development and best practices, problem-solving, and experience handling complex requirements. Please format the output strictly as an array of JSON objects without any additional labels, code blocks, or explanations. Return only the JSON array with questions and answers.
+        `;
+        //rawAiResult is the response from the AI service, which contains the generated interview questions and answers.
+        const rawAiResult = await chatSession.sendMessage(prompt);
+        // console.log(rawAiResult.response.text().trim());
+        const cleanedResponse = cleanAiResponse(rawAiResult.response.text().trim());
+        console.log(cleanedResponse);
+
+        return cleanedResponse;
+    }
     const onSubmit = async (data: FormData) => {
         try {
             setLoading(true);
+            if (initialData) {
+                //update
+
+                const aiResult = await generateAiResponse(data);
+
+                await updateDoc(doc(db, "interviews", initialData?.id), {
+                    questions: aiResult,
+                    ...data,
+                    updatedAt: serverTimestamp(),
+                })
+                toast.success(toastMessage.title, {
+                    description: toastMessage.description
+                })
+            } else {
+
+                //Create new interview
+                if (isValid) {
+                    // aiResult is cleaned response
+                    const aiResult = await generateAiResponse(data);
+
+                    await addDoc(collection(db, "interviews"), {
+                        ...data,
+                        userId,
+                        questions: aiResult,
+                        createdAt: serverTimestamp(),
+                    })
+
+                    toast.success(toastMessage.title, { description: toastMessage.description });
+
+
+
+
+                }
+            }
+            navigate("/generate", { replace: true });
+            console.log("Form Data: ", data);
         } catch (error) {
             console.log(error);
             toast.error("Error..", {
@@ -97,7 +182,13 @@ const FormMockInterview = ({ initialData }: FormMockInterviewProps) => {
 
             <Separator className="my-4"></Separator>
 
-            <div className="my-6"></div>
+
+            <div className="my-6">
+                <Button onClick={() =>
+                    alert("Button Clicked!")
+                }></Button>
+            </div>
+
 
             {/* spread operators expands an iterable (like an array) into more elements. */}
             {/* spread operators are used to pass the form methods to the FormProvider component, which allows us to use the form methods in the child components. */}
@@ -108,7 +199,7 @@ const FormMockInterview = ({ initialData }: FormMockInterviewProps) => {
                     <FormField
                         control={form.control}
                         name="position"
-                        render={({ ...field }) => (
+                        render={({ field }) => (
                             <FormItem className="w-full space-y-4">
                                 <div className="w-full flex items-center justify-between">
                                     <FormLabel>
@@ -118,10 +209,12 @@ const FormMockInterview = ({ initialData }: FormMockInterviewProps) => {
                                 </div>
                                 <FormControl>
                                     <Input
-                                        {...field}
                                         disabled={loading}
                                         className="h-12"
-                                        placeholder="eg: ML- engineer" />
+                                        placeholder="eg: ML- engineer"
+                                        {...field}
+                                        value={field.value || ""}
+                                    />
                                 </FormControl>
                             </FormItem>
                         )}
@@ -132,7 +225,7 @@ const FormMockInterview = ({ initialData }: FormMockInterviewProps) => {
                     <FormField
                         control={form.control}
                         name="description"
-                        render={({ ...field }) => (
+                        render={({ field }) => (
                             <FormItem className="w-full space-y-4">
                                 <div className="w-full flex items-center justify-between">
                                     <FormLabel>
@@ -142,10 +235,12 @@ const FormMockInterview = ({ initialData }: FormMockInterviewProps) => {
                                 </div>
                                 <FormControl>
                                     <Textarea
-                                        {...field}
                                         disabled={loading}
                                         className="h-12"
-                                        placeholder="eg: Describe your job role" />
+                                        placeholder="eg: Describe your job role"
+                                        {...field}
+                                        value={field.value || ""}
+                                    />
                                 </FormControl>
                             </FormItem>
                         )}
@@ -155,7 +250,7 @@ const FormMockInterview = ({ initialData }: FormMockInterviewProps) => {
                     <FormField
                         control={form.control}
                         name="experience"
-                        render={({ ...field }) => (
+                        render={({ field }) => (
                             <FormItem className="w-full space-y-4">
                                 <div className="w-full flex items-center justify-between">
                                     <FormLabel>
@@ -165,10 +260,12 @@ const FormMockInterview = ({ initialData }: FormMockInterviewProps) => {
                                 </div>
                                 <FormControl>
                                     <Input
-                                        {...field}
                                         disabled={loading}
                                         className="h-12"
-                                        placeholder="eg: Enter your experience" />
+                                        placeholder="eg: Enter your experience"
+                                        {...field}
+                                        value={field.value || ""}
+                                    />
                                 </FormControl>
                             </FormItem>
                         )}
@@ -178,7 +275,7 @@ const FormMockInterview = ({ initialData }: FormMockInterviewProps) => {
                     <FormField
                         control={form.control}
                         name="techStack"
-                        render={({ ...field }) => (
+                        render={({ field }) => (
                             <FormItem className="w-full space-y-4">
                                 <div className="w-full flex items-center justify-between">
                                     <FormLabel>
@@ -188,14 +285,25 @@ const FormMockInterview = ({ initialData }: FormMockInterviewProps) => {
                                 </div>
                                 <FormControl>
                                     <Input
-                                        {...field}
                                         disabled={loading}
                                         className="h-12"
-                                        placeholder="eg: Your preferred tech stack" />
+                                        placeholder="eg: Your preferred tech stack"
+                                        {...field}
+                                        value={field.value || ""}
+                                    />
                                 </FormControl>
                             </FormItem>
                         )}
                     ></FormField>
+
+                    <div className=" w-full flex items-center justify-end gap-6">
+                        <Button type="reset" size={"sm"} variant={"outline"} disabled={isSubmitting || loading} onClick={() => { form.reset(); toast("Form reset successful") }}>
+                            Reset
+                        </Button>
+                        <Button type="submit" size={"sm"} variant={"outline"} disabled={isSubmitting || loading || !isValid}>
+                            {loading ? <Loader className="text-gray-50 animate-spin" /> : actions}
+                        </Button>
+                    </div>
                 </form>
             </FormProvider>
         </div >
