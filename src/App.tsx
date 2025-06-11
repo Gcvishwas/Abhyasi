97304d76825df68fad36dd48c553401438efaefa
Gcvishwas <gcvishwas17@gmail.com>
2025-06-11 18:36:51 +0545

    Added Contact, About, and Services routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import PublicLayout from "@/layouts/public-layout"
-import HomePage from "@/routes/home"
-import AuthenticationLayout from "@/layouts/auth-layout"
-import SignInPage from "@/routes/sign-in"
-import SignUpPage from "@/routes/sign-up"
-import ProtectRoutes from "@/layouts/protected-routes"
-import MainLayout from "@/layouts/main-layout"
-import Generate from "./components/generate"
-import Dashboard from "./routes/dashboard"
-import CreateEditPage from "./routes/createEditPage"
-import MockLoadPage from "./routes/mock_loadpage"
-import MockInterviewPage from "./routes/mock-interview-page"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PublicLayout from "@/layouts/public-layout";
+import HomePage from "@/routes/home";
+import AuthenticationLayout from "@/layouts/auth-layout";
+import SignInPage from "@/routes/sign-in";
+import SignUpPage from "@/routes/sign-up";
+import ProtectRoutes from "@/layouts/protected-routes";
+import MainLayout from "@/layouts/main-layout";
+import Generate from "./components/generate";
+import Dashboard from "./routes/dashboard";
+import CreateEditPage from "./routes/createEditPage";
+import MockLoadPage from "./routes/mock_loadpage";
+import MockInterviewPage from "./routes/mock-interview-page";
+import Contact from "./routes/contact";
+import About from "./routes/About";
+import Services from "./routes/Services";
 
 const App = () => {
   return (
@@ -19,6 +22,9 @@ const App = () => {
         {/* public routes */}
         <Route element={<PublicLayout />}>
           <Route index element={<HomePage />} />
+          <Route element={<Contact />} path="/contact" />
+          <Route element={<About />} path="/about" />
+          <Route element={<Services />} path="/services" />
         </Route>
 
         {/* authentication layout */}
@@ -28,20 +34,28 @@ const App = () => {
         </Route>
 
         {/* private routes */}
-        <Route element={<ProtectRoutes><MainLayout /></ProtectRoutes>}>
-
+        <Route
+          element={
+            <ProtectRoutes>
+              <MainLayout />
+            </ProtectRoutes>
+          }
+        >
           {/* add all the private routes */}
 
           <Route element={<Generate />} path="/generate">
             <Route index element={<Dashboard />} />
             <Route path=":interviewId" element={<CreateEditPage />} />
             <Route path="interview/:interviewId" element={<MockLoadPage />} />
-            <Route path="interview/:interviewId/start" element={<MockInterviewPage />} />
+            <Route
+              path="interview/:interviewId/start"
+              element={<MockInterviewPage />}
+            />
           </Route>
         </Route>
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
