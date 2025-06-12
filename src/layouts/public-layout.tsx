c5227fbd1cc93ce0a7ed4bd712835d3a875a264a
Gcvishwas <gcvishwas17@gmail.com>
2025-06-12 20:56:45 +0545

    Done basic styling changes

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,18 +1,19 @@
-
-import { Outlet } from 'react-router-dom'
-import AuthHandler from '@/handlers/auth-handler'
-import Header from '@/components/header'
-import Footer from '@/components/footer'
+import { Outlet } from "react-router-dom";
+import AuthHandler from "@/handlers/auth-handler";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 const PublicLayout = () => {
-    return (
-        <div className='w-full'>
-            {/* handler to store the user data */}
-            <AuthHandler />
-            <Header />
-            <Outlet />
-            <Footer />
-        </div>
-    )
-}
+  return (
+    <div className="w-full min-h-screen flex flex-col">
+      {/* handler to store the user data */}
+      <AuthHandler />
+      <Header />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
-export default PublicLayout
+export default PublicLayout;
