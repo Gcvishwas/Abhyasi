7619814d981d50b09b97c9d03f16daf1da0870bd
Gcvishwas <gcvishwas17@gmail.com>
2025-06-12 20:51:46 +0545

    Refactor Container component for improved readability and consistency

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,14 +1,19 @@
 import { cn } from "@/lib/utils";
 interface ContainerProps {
-    children: React.ReactNode;
-    className?: string;
+  children: React.ReactNode;
+  className?: string;
 }
 
 const Container = ({ children, className }: ContainerProps) => {
-    return (
-        <div className={cn("container mx-auto px- md:px-8 py-4 w-full", className)}>
-            {children}
-        </div >
-    );
+  return (
+    <div
+      className={cn(
+        "container mx-auto max-w-7xl px-4 md:px-8 py-4 w-full",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
 };
-export default Container
+export default Container;
